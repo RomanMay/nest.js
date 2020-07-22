343ccc5b9c2909129d861711d57ad93f27f9dcfa
RomanMay <romano19991512@gmail.com>
2020-07-22 16:45:43 +0300

    add users

--- src/tasks/task.rapositiry.ts
+++ src/tasks/task.rapositiry.ts
@@ -3,13 +3,17 @@ import{ EntityRepository, Repository} from 'typeorm'
 import { CreateTaskDto } from './dto/create-task.dto'
 import { TaskStatus } from './task-status.enum'
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto'
+import { User } from 'src/auth/user.entity'
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
-    async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    async getTasks(
+        filterDto: GetTasksFilterDto,
+        user: User
+        ): Promise<Task[]> {
         const {status, search} = filterDto
         const query = this.createQueryBuilder('task')
-
+        query.where('task.userId = :userId', {userId: user.id})
         if(status) {
             query.andWhere('task.status = :status', {status})
         }
@@ -22,14 +26,20 @@ export class TaskRepository extends Repository<Task> {
         return task
     }
 
-    async createTast(createTaskDto: CreateTaskDto) {
+    async createTask(
+        createTaskDto: CreateTaskDto,
+        user: User
+        ) {
         const {title, description} = createTaskDto
         
         const task = new Task()
         task.title = title
         task.description = description
         task.status = TaskStatus.OPEN
+        task.user = user
+
         await task.save()
+        delete task.user
 
         return task
     }
